fdc70603f7c68489a0fb7ea955c8a242addb268c
Jerad Bitner <sirkitree@gmail.com>
2013-03-10 19:16:00 -0600

    pulling in all the mysql work as functions to call from here.

--- routes/api.js
+++ routes/api.js
@@ -17,15 +17,8 @@ exports.ldapLoad = function (req, res) {
   ldapLoad(req, res);
 };
 
-function karmaSave (data, filepath) {
-  var fs = require('fs');
-
-  // Write the data to file for quick retrieval.
-  fs.writeFile(filepath, JSON.stringify({"nicks": data}, null, 4), function(err) {
-    if (err) { console.log(err); return; } 
-    console.log("The file was saved!");  
-  });
-
+exports.mysqlLoad = function (req, res) {
+  mysqlLoad(req, res);
 };
 
 function ldapLoad (request, result) {
@@ -59,13 +52,115 @@ function ldapLoad (request, result) {
 
     // Save to file for quick retrieval next call.
     res.on('end', function(res) {
-      karmaSave(nicks, './routes/data.json');
-
       // Return the data from the api call.
-      result.json({
-        "nicks": nicks
-      });
+      mysqlLoad(nicks, result);
     });
 
   });
-}
+};
+
+function mysqlLoad (nicks, result) {
+  var walk = require('walk')
+    , fs = require('fs')
+    , dirwalk = walk.walk('/var/www')
+    , runner = require('child_process')
+    , mysql = require('mysql')
+    , names = [];
+
+  for (var i = nicks.length - 1; i >= 0; i--) {
+    // Reset all Karma to 0 so we're only getting new values.
+    nicks[i].karma = 0;
+
+    // Used in our query creation.
+    names.push(nicks[i].name);
+  }
+
+  // Create our query from our currenty list of nicks.
+  var query = 'select * from bot_karma where term in("' + names.join('","') + '")';
+
+  // Go through each directory in /var/www.
+  dirwalk.on('directories', function (root, dirStatsArray, next) {
+
+    for (var i = dirStatsArray.length - 1; i >= 0; i--) {
+      if (dirStatsArray[i].type === 'directory') {
+
+        // GO through each settings.php file in these directories.
+        filewalk = walk.walk(root + '/' + dirStatsArray[i].name);
+        filewalk.on('file', function (root, fileStats, next) {
+
+          // Execute php on the host machine to turn the database array
+          // into a json object which we can work with.
+          runner.exec(
+            'php -r \'include("' + root + '/' + fileStats.name + '"); print json_encode($databases);\'', 
+            function (err, stdout, stderr) {
+
+              // Parse stdout into JSON.
+              var connectTo = JSON.parse(stdout).default.default
+                // Use our parsed variables to establish a connection to mysql.
+                , connection = mysql.createConnection({
+                  host: connectTo.host,
+                  user: connectTo.username,
+                  password: connectTo.password,
+                  database: connectTo.database
+                });
+
+              connection.connect();
+
+              // Run our previously crafted query.
+              connection.query(query, function (err, rows, fields) {
+                if (err) {
+                  console.log(err);
+                } else {
+                  // Add the newly retrieved karma to an array
+                  addEmUp(rows, nicks);
+                }
+
+              });
+              connection.end();
+
+              next();
+            }
+          );
+        
+        });
+      }
+    };
+
+    next();
+  });
+
+  // Return nicks to our result.
+  dirwalk.on('end', function() {
+    result.json({
+      "nicks": nicks
+    });
+  });
+};
+
+function addEmUp(rows, nicks) {
+  // Iterate over the rows returned from mysql.
+  for (var i = rows.length - 1; i >= 0; i--) {
+    // Iterate over our nicks.
+    for (var j = nicks.length - 1; j >= 0; j--) {
+      // If the current term is in our nicks (which is should be cuz that's 
+        // what we started with) add up the karma points.
+      if (rows[i].term === nicks[j].name) {
+        // update the karma
+        nicks[j].karma += parseInt(rows[i].karma);
+      }
+    };
+  };
+
+  karmaSave(nicks, './routes/data.json');
+};
+
+function karmaSave (data, filepath) {
+  var fs = require('fs');
+
+  // Write the data to file for quick retrieval.
+  fs.writeFile(filepath, JSON.stringify({"nicks": data}, null, 4), function(err) {
+    if (err) { console.log(err); return; } 
+    console.log("The file was saved!");  
+  });
+
+};
